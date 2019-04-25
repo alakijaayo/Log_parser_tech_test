cbf0c828944b8c2e4e4b79502b3cc221f3b484d0
alakijaayo <alakijaayo@gmail.com>
2019-04-25 17:07:28 +0100

    Implementing Reader Class

--- lib/reader.rb
+++ lib/reader.rb
@@ -1,6 +1,25 @@
 class Reader
+  attr_reader :file_path, :entries
 
-  def initialize
+  def initialize(file_path)
+    @file_path = file_path
+    @entries = []
+  end
 
+  def call
+    with_file do |line|
+      url, ip = line.split
+      @entries << { url: url, ip: ip }
+    end
+  end
+
+  private
+
+  def with_file
+    File.open(file_path, 'r') do |file|
+      while (line = file.gets)
+        yield line
+      end
+    end
   end
 end
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -1,6 +1,24 @@
 require 'spec_helper'
 require 'reader'
 
-describe Player do
-  
+describe Reader do
+  subject { described_class.new(file_path) }
+
+  context "when file exists" do
+    let(:file_path) { "spec/fixtures/20_lines.log" }
+
+    before { subject.call }
+
+    it "reads ten log entries" do
+      expect(subject.entries.count).to eq 20
+    end
+  end
+
+  context "when file does not exist" do
+    let(:file_path) { "spec/fixtures/new_file.log" }
+
+    it "raises an error message" do
+      expect { subject.call }.to raise_error(Errno::ENOENT)
+    end
+  end
 end
