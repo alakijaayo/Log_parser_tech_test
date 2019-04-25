c4fef795e9801351b5632019dec19d60f29d19d3
alakijaayo <alakijaayo@gmail.com>
2019-04-25 22:45:55 +0100

    Implementing Parser Class

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,6 +1,32 @@
-class Parser
+$LOAD_PATH << File.dirname(__FILE__)
 
-  def initialize
-    
+require "reader"
+require "EntryData/unique_visits"
+require "EntryData/number_of_visits"
+
+class Parser
+  attr_reader :log_entries
+
+  def initialize(log_entries)
+    @log_entries = log_entries
   end
+
+  def show_most_popular(unique: false)
+    strategy_class = unique ? EntryData::UniqueVisit : EntryData::Visit
+    strategy_class.new(log_entries).call
+  end
+end
+
+log_file_path = ARGV[0]
+
+if log_file_path
+  reader = Reader.new(log_file_path)
+  reader.call
+
+  parser = Parser.new(reader.log_entries)
+  parser.show_most_popular
+  puts "-----------"
+  parser.show_most_popular(unique: true)
+else
+  puts "USAGE: ./parser.rb PATH_TO_LOG_FILE"
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,6 +1,30 @@
 require "spec_helper"
 require "parser"
 
-describe Parser do
-  
+RSpec.describe Parser do
+  subject { described_class.new([]) }
+
+  describe "#show_most_popular" do
+    let(:strategy_object) { instance_double EntryData::Base }
+
+    before { allow(strategy_class).to receive(:new).and_return(strategy_object) }
+
+    context "when calls with unique set to true" do
+      let(:strategy_class) { EntryData::UniqueVisit }
+
+      it "calls PopularityStrategy::UniqueVisit" do
+        expect(strategy_object).to receive(:call)
+        subject.show_most_popular(unique: true)
+      end
+    end
+
+    context "when calls without unique" do
+      let(:strategy_class) { EntryData::Visit }
+
+      it "calls PopularityStrategy::Visit" do
+        expect(strategy_object).to receive(:call)
+        subject.show_most_popular
+      end
+    end
+  end
 end
